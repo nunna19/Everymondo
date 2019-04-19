f37b4785e524069d1eaf8f2c027c1a930194247b
nunna19 <nunnapat_ch@yahoo.com>
2019-04-18 23:08:12 -0400

    relax

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
+import { Route, Link, NavLink, Switch } from 'react-router-dom';
+import 'react-notifications/lib/notifications.css';
+import 'rc-slider/assets/index.css';
+
 import './App.scss';
 import Home from './Components/Home.js';
 import FormSubmit from './Components/FormSubmit';
+
+
+
 
 
 class App extends Component {
@@ -9,8 +16,13 @@ class App extends Component {
     return (
       <div className="App">
 
-     <FormSubmit/>
-    
+      <Switch>
+          <Route
+            path='/FormSubmit'
+            render={(props) => <FormSubmit {...props} setUser={this.setUser}/>}
+          />
+      </Switch>
+
       <Home/>
       
         
